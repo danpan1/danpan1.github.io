1c1bbb82a2f9e19b583fe53cf1b3729d5ee97553
Панкрашин Данил <danpan@yandex.ru>
2016-02-29 18:13:12 +0300

    upd fork me

--- frontend/home.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import welcome from './welcome';
-// import testTmpl from './test.html';
-welcome("home");
-// let test = './test.html'
-
-exports.welcome = welcome;
-exports.test = require('./test.html');
